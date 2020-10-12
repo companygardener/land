08b6e7dec1047923ea2c333adcbd06a2534ab714
Steven Kim <stevenskim@users.noreply.github.com>
2020-10-12 13:44:57 -0500

    fix invalid association to correct ones (#10)
    Co-authored-by: Steven Kim <skim@iloan.com>

--- app/lookups/land/query_string.rb
+++ app/lookups/land/query_string.rb
@@ -4,7 +4,7 @@ module Land
 
     lookup_by :query_string, cache: 50, find_or_create: true, allow_blank: true
 
-    has_many :page_views
+    has_many :pageviews
     has_many :referers
   end
 end
