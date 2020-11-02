9c7d47263d5a0bcf90fcebd3affe1c14aeed89c3
Steven Kim <stevenskim@users.noreply.github.com>
2020-11-02 15:32:58 -0600

    enable find_or_create on lookup EvenType (#13)
    Co-authored-by: Steven Kim <skim@iloan.com>

--- app/lookups/land/event_type.rb
+++ app/lookups/land/event_type.rb
@@ -2,7 +2,7 @@ module Land
   class EventType < ApplicationRecord
     include TableName
 
-    lookup_by :event_type, cache: 100
+    lookup_by :event_type, cache: 100, find_or_create: true
 
     has_many :events
   end
