7df1e1cbbbaa335b12a0bb84524067550900e6f4
ashzagorski <41012710+ashzagorski@users.noreply.github.com>
2021-10-13 10:52:24 -0500

    missing belongs_to (#17)

--- app/models/land/event.rb
+++ app/models/land/event.rb
@@ -5,6 +5,7 @@ module Land
 
     lookup_for :event_type, class_name: EventType, symbolize: true
     
+    belongs_to :event_type, required: false
     belongs_to :pageview, required: false
 
     scope :by_event_type, -> event_type {find_by(event_type: event_type)}
