1376063c46edcf060c3af4bdcf66b9539b043a89
Roger Cosseboom <rogercosseboom@gmail.com>
2022-11-10 08:33:46 -0800

    Update migrations to be conditional (#25)
    Co-authored-by: Roger Cosseboom <rcosseboom@beyondfinance.com>

--- db/migrate/20220914012158_add_indexes_to_events.rb
+++ db/migrate/20220914012158_add_indexes_to_events.rb
@@ -2,7 +2,7 @@ class AddIndexesToEvents < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def change
-    add_index "#{Land.config.schema}.events", :created_at, algorithm: :concurrently
-    add_index "#{Land.config.schema}.events", :pageview_id, algorithm: :concurrently
+    add_index "#{Land.config.schema}.events", :created_at, algorithm: :concurrently unless index_exists?("#{Land.config.schema}.events", :created_at)
+    add_index "#{Land.config.schema}.events", :pageview_id, algorithm: :concurrently unless index_exists?("#{Land.config.schema}.events", :pageview_id)
   end
 end
